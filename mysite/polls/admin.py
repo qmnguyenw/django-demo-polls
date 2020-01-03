4c6734e98be38c2e13c6fbffad404e8aacd789f7
Quang Nguyen <qmnguyenw@gmail.com>
2020-01-03 05:48:16 -0500

    upload

---
+++ mysite/polls/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+# Step 1
+# admin.site.register(Question)
+
+# Step 2
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+# admin.site.register(Question, QuestionAdmin)
+
+# Step 3
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']}),
+#     ]
+# admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
+
+# Step 4
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 
+    'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 3
+    date_hierarchy = 'pub_date'
+
+admin.site.register(Question, QuestionAdmin)
